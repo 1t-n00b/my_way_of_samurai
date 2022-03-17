17bf09e4e42cd351dffec23634a4b17187d4f8bf
1t-n00b <mummintrol@gmail.com>
2022-03-17 16:16:10 +0200

    39 refactor

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -4,28 +4,25 @@ import ProfileInfo from "./MyPosts/Posts/ProfileInfo/ProfileInfo";
 import {PostsType} from "../../App";
 import {ActionsTypes} from "../../redux/state";
 
-
 type ProfileType = {
     profilePage: {
         posts: Array<PostsType>
         newPostText: string
     },
-   // addPost: (somePost: string) => void
-  //  updateNewPostText: (newText: string) => void
-    dispatch:  (action: ActionsTypes) => void
+    // addPost: (somePost: string) => void
+    //  updateNewPostText: (newText: string) => void
+    dispatch: (action: ActionsTypes) => void
 }
 
-
 function Profile(props: ProfileType) {
-
 
     return (
         <div>
             <ProfileInfo/>
             <MyPosts post={props.profilePage.posts}
                      newPostText={props.profilePage.newPostText}
-                     // addPost={props.addPost}
-                     // updateNewPostText={props.updateNewPostText}
+                // addPost={props.addPost}
+                // updateNewPostText={props.updateNewPostText}
                      dispatch={props.dispatch}
             />
 
